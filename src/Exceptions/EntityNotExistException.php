135b288470c7e9fd0eceaac7dc99b86ff9c41de5
ahmedJS <ahmed_alsadi56@outlook.com>
2024-03-13 21:50:45 +0300

    change and enhance  EntityNotExistException.php a little bit

--- src/Exceptions/EntityNotExistException.php
+++ src/Exceptions/EntityNotExistException.php
@@ -3,4 +3,10 @@ namespace Vekas\EntityService\Exceptions;
 
 class EntityNotExistException extends \Exception {
 
+    private $entityName;
+    function __construct($entityName) {
+        $this->entityName = $entityName;
+        $this->code = 404;
+        $this->message = "the field $entityName was not set";
+    }
 }
